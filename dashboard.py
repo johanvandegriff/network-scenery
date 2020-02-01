7389726bbd779f0bcbeb1a934f9251a2fe080e30
Simran Sethi <simransethi1@yahoo.com>
2020-02-01 16:26:12 -0500

    improve dashboard.

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,30 @@
 from flask import Flask, request, render_template, url_for
-
+from networkScanner import scan
 import os, re, sys
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    #return "<h1>hi!</h1>"
-    return render_template("index.html", text="some text")
 
-@app.route("/test")
+    data =[['10.128.128.1', '00:2f:5c:03:61:50', '(Unknown)'],
+     ['10.128.128.50', 'b8:7b:c5:07:69:89', '(Unknown)'],
+     ['10.128.128.50', 'b8:7b:c5:07:69:89', '(Unknown) (DUP: 2)']]
+
+    #data = scan("wlan0")
+
+
+
+
+    table = ""
+    for line in data:
+        table += "<tr>"
+        for info in line:
+            table += "<td>" + info + "</td>\n"
+        table+= "</tr>"
+    return render_template("index.html", data=table)
+
+@app.route("/profile")
 def test():
     return "<h1>test123</h1>"
 
--- networkScanner.py
+++ networkScanner.py
@@ -1,23 +1,25 @@
+#!/usr/bin/python3
 import subprocess
-from sense_hat import SenseHat
 
 # this function will return a list of lists describing the list of devices in the network
 # format: [IP, MAC, NAME/MANUFACTURER]
 # find out interface by running ifconfig and determining which interface is being used, like eth0 or wlp3s0
 def scan(interface):
-  # use subprocess.run to run the command 
-  cliOutput = subprocess.run(['sudo', 'arp-scan', '--localnet', '--interface=' + interface], stdout=subprocess.PIPE)
-  # extract the output from the subprocess
-  strOutput = cliOutput.stdout.decode()
-  # generate a list of lists in the specified format and return it
-  #return strOutput
-  return [i.split('\t') for i in strOutput.split('\n')][2:-4]
+    # use subprocess.run to run the command
+    cliOutput = subprocess.run(['sudo', 'arp-scan', '--localnet', '--interface=' + interface], stdout=subprocess.PIPE)
+    # extract the output from the subprocess
+    strOutput = cliOutput.stdout.decode()
+    # generate a list of lists in the specified format and return it
+    #return strOutput
+    return [i.split('\t') for i in strOutput.split('\n')][2:-4]
 
-sense = SenseHat()
+if __name__ == "__main__":
+    from sense_hat import SenseHat
+    sense = SenseHat()
 
-s = scan("wlan0")
-print(len(s))
-print(s[0])
+    s = scan("wlan1")
+    print(len(s))
+    print(s[0:3])
 
-sense.clear()
-sense.set_pixel(2, 2, (0, min(len(s), 255), 0))
+    sense.clear()
+    sense.set_pixel(2, 2, (0, min(len(s), 255), 0))
